f6a3cc5dce927bba5a5603c39e7a7357f1f06068
danjkelman <46281017+danjkelman@users.noreply.github.com>
2019-01-10 18:47:39 +0900

    Update heapsort.py

--- heapsort.py
+++ heapsort.py
@@ -1,13 +1,22 @@
-#incomplete will come back to soon but step one is out of the way
 heap=[]
-seq=[[1,3],[1,4],[2,5],[2,6],[0,1],[0,2]]
+seq=[[2,6],[2,5],[1,4],[1,3],[0,2],[0,1]]
+finalList=[]
 
 for i in range(7):
     heap.append(int(input("input interger: ")))
-print(heap)
-for i in range(len(seq)):
-    seq2=seq[i]
-    if heap[seq2[0]]<heap[seq2[1]]:
-        heap[seq2[0]], heap[seq2[1]] = heap[seq2[1]], heap[seq2[0]]
-        print(heap)
-print("done",heap)
+
+while len(seq)>0:
+    for x in range(len(heap)):
+        for i in range(len(seq)):
+            seq2=seq[i]
+            if heap[seq2[0]]>heap[seq2[1]]:
+                heap[seq2[0]], heap[seq2[1]] = heap[seq2[1]], heap[seq2[0]]
+    finalList.append(heap[0])
+    del heap[0]
+    test = 1
+    test = test + 1
+    if test%2==0:
+        del seq[0]
+finalList.append(heap[0])
+    
+print("done",finalList)
